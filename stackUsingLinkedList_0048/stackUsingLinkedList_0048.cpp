ace09db4ed8899f374088132f4825af9f0d3d014
nachool <nachma.olivia1394@gmail.com>
2024-05-11 09:37:21 +0700

    menambah class stack

--- stackUsingLinkedList_0048/stackUsingLinkedList_0048.cpp
+++ stackUsingLinkedList_0048/stackUsingLinkedList_0048.cpp
@@ -4,6 +4,17 @@ using namespace std;
 //Node class representing a single node in the linked list
 class Node {
 public:
+	int data;
+	Node* next;
+
+	Node() {
+		next = NULL;
+	}
+};
+
+//stack class
+class stack {
+
 };
 
 int main()
